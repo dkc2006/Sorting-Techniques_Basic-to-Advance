217179125443c84037abd7675025d9a2226f8ac8
Deepanshu Khandelwal <khandelwaldeepanshu6@gmail.com>
2024-03-15 16:56:54 +0530

    Initial Commit

---
+++ src/main/java/sorting/bubbleSort/StringWithIntegerSort.java
@@ -0,0 +1,49 @@
+package sorting.bubbleSort;
+
+import java.util.Arrays;
+
+class MobilePhone {
+    int id;
+    String name;
+    Double price;
+
+
+    public MobilePhone(String name,Double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "MobilePhone{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
+class Sorter{
+    public void sort(MobilePhone [] phones){
+        for (int counter = 0;counter<phones.length;counter++) {
+            for (int index = 0; index < phones.length-1; index++) {
+                if (phones[index].price > phones[index + 1].price) {
+                    MobilePhone temp = phones[index];
+                    phones[index] = phones[index+1];
+                    phones[index+1] = temp;
+                }
+            }
+        }
+    }
+}
+class Main{
+    public static void main(String[] args) {
+        MobilePhone []phones = new MobilePhone[]{new MobilePhone("A", 10.0), new MobilePhone("B", 4.0)};
+        System.out.println(Arrays.toString(phones));
+        Sorter sorter =new Sorter();
+        sorter.sort(phones);
+        System.out.println(Arrays.toString(phones));
+
+    }
+}
