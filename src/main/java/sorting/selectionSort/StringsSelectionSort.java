4e37047af4e8392a3226bc2b9967689e97c21801
Deepanshu Khandelwal <khandelwaldeepanshu6@gmail.com>
2024-03-16 17:24:14 +0530

    Third commit 2

---
+++ src/main/java/sorting/selectionSort/StringsSelectionSort.java
@@ -0,0 +1,28 @@
+package sorting.selectionSort;
+
+import java.util.Arrays;
+
+public class StringsSelectionSort {
+    public static void selectionSort(String [] names){
+        for (int index = 0; index < names.length - 1; index++) {
+            int minpos = index;
+            for(int j = index+1;j< names.length;j++){
+                if(names[minpos].compareTo(names[j]) > 0){
+                    minpos = j;
+                }
+            }
+            //Swap
+            String temp = names[minpos];
+            names[minpos] = names[index];
+            names[index] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] names = new String[]{"Kanu","Deepanshu","Shyamji","Bholenath","Ramji"};
+        System.out.println("Given names are : " + Arrays.toString(names));
+        StringsSelectionSort stringsSelectionSort = new StringsSelectionSort();
+        stringsSelectionSort.selectionSort(names);
+        System.out.println("Sorted names are : " + Arrays.toString(names));
+    }
+}
