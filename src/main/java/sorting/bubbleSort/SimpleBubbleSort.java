3bef1040c2c2483e23f9ba5963409a6e132ee9a7
Deepanshu Khandelwal <khandelwaldeepanshu6@gmail.com>
2024-03-16 17:05:07 +0530

    Third commit

--- src/main/java/sorting/bubbleSort/SimpleBubbleSort.java
+++ src/main/java/sorting/bubbleSort/SimpleBubbleSort.java
@@ -15,7 +15,6 @@ public class SimpleBubbleSort {
         }
 
     }
-
     public static void main(String[] args) {
         int [] numbers = new int[]{5,2,8,9,1,3,7,4};
         System.out.println("Given numbers are : " + Arrays.toString(numbers));
@@ -23,8 +22,6 @@ public class SimpleBubbleSort {
         sorter.sort(numbers);
         System.out.println("Sorted numbers are : " + Arrays.toString(numbers));
     }
-
-
 }
 
 
--- src/main/java/sorting/selectionSort/SimpleSelectionSort.java
+++ src/main/java/sorting/selectionSort/SimpleSelectionSort.java
@@ -1,4 +1,29 @@
 package sorting.selectionSort;
 
+import java.util.Arrays;
+
 public class SimpleSelectionSort {
+    public static void selectionSort(int [] numbers){
+        for (int index = 0; index < numbers.length - 1; index++) {
+            int minpos = index;
+            for (int j = index+1; j < numbers.length; j++) {
+                if(numbers[minpos] > numbers[j]){
+                    minpos = j;
+                }
+            }
+            //Swap
+            int temp = numbers[minpos];
+            numbers[minpos] = numbers[index];
+            numbers[index] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int [] numbers = new int[]{8,5,7,4,6,3,1,2};
+        System.out.println("Given numbers are : " + Arrays.toString(numbers));
+        SimpleSelectionSort sort = new SimpleSelectionSort();
+         sort.selectionSort(numbers);
+        System.out.println("Sorted numbers are : " + Arrays.toString(numbers));
+
+    }
 }
