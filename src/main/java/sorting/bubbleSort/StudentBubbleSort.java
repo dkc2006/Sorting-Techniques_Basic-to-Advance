350144564034774caa8216c1e261ee26d6933dce
Deepanshu Khandelwal <khandelwaldeepanshu6@gmail.com>
2024-03-20 11:16:32 +0530

    Third commit 2

--- src/main/java/sorting/bubbleSort/StudentBubbleSort.java
+++ src/main/java/sorting/bubbleSort/StudentBubbleSort.java
@@ -26,20 +26,15 @@ public class StudentBubbleSort {
 }
 
 class Sorter1 {
-    public void sort(StudentBubbleSort[] numbers) {
-        for (int counter = 0; counter < numbers.length; counter++) {
-            for (int index = 0; index < numbers.length - 1; index++) {
-                if (numbers[index].rollNumber > numbers[index + 1].rollNumber) {
-                    StudentBubbleSort temp = numbers[index];
-                    numbers[index] = numbers[index + 1];
-                    numbers[index + 1] = temp;
+    public void sort(StudentBubbleSort[] student) {
+        for (int counter = 0; counter < student.length; counter++) {
+            for (int index = 0; index < student.length - 1; index++) {
+                if (student[index].rollNumber > student[index + 1].rollNumber) {
+                    StudentBubbleSort temp = student[index];
+                    student[index] = student[index + 1];
+                    student[index + 1] = temp;
                 }
             }
         }
     }
 }
-
-
-
-
-
