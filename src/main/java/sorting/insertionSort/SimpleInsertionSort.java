a10c7dec51fc52621ad47a88ab334ebad10de3b7
Deepanshu Khandelwal <khandelwaldeepanshu6@gmail.com>
2024-03-29 16:13:23 +0530

    Third commit 2

--- src/main/java/sorting/insertionSort/SimpleInsertionSort.java
+++ src/main/java/sorting/insertionSort/SimpleInsertionSort.java
@@ -1,4 +1,25 @@
 package sorting.insertionSort;
 
+import java.util.Arrays;
+
 public class SimpleInsertionSort {
+    public static void main(String[] args) {
+        int[] numbers = new int[]{5, 2, 8, 9, 1, 3, 7, 4};
+        System.out.println("Given numbers are : " + Arrays.toString(numbers));
+        SimpleInsertionSort simpleInsertionSort = new SimpleInsertionSort();
+        simpleInsertionSort.insertionSort(numbers);
+        System.out.println("Sorted numbers are : " + Arrays.toString(numbers));
+    }
+
+    void insertionSort(int[] numbers) {
+        for (int index = 1; index < numbers.length; index++) {
+            int currentElement = numbers[index]; // corrected from numbers[1] to numbers[index]
+            int comparingIndex = index - 1;
+            while (comparingIndex >= 0 && currentElement < numbers[comparingIndex]) {
+                numbers[comparingIndex + 1] = numbers[comparingIndex];
+                comparingIndex--;
+            }
+            numbers[comparingIndex + 1] = currentElement; // corrected from numbers[currentElement] to currentElement
+        }
+    }
 }
