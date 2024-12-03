091b22dc8072cacfe2ec2d1b69100f52cee5f84b
Deepanshu Khandelwal <122585913+dkc2006@users.noreply.github.com>
2024-12-03 20:47:43 +0530

    Quick Sort

--- src/main/java/sorting/quickSort/SimpleQuickSort.java
+++ src/main/java/sorting/quickSort/SimpleQuickSort.java
@@ -2,8 +2,22 @@ package sorting.quickSort;
 
 // Java program for implementation of QuickSort
 public class SimpleQuickSort {
+    // Driver method
+    public static void main(String[] args) {
+        int[] a = {10, 7, 8, 9, 1, 5};
+        int n = a.length;
+
+        SimpleQuickSort ob = new SimpleQuickSort();
+        ob.sort(a, 0, n - 1);
+
+        System.out.println("Sorted array:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+
     // Function to partition the array
-    int partition(int a[], int low, int high) {
+    int partition(int[] a, int low, int high) {
         int pivot = a[high];
         int i = (low - 1); // Index of smaller element
 
@@ -27,27 +41,13 @@ public class SimpleQuickSort {
     }
 
     // Function to implement QuickSort
-    void sort(int a[], int low, int high) {
+    void sort(int[] a, int low, int high) {
         if (low < high) {
             int pi = partition(a, low, high);
 
             // Recursively sort elements before and after partition
             sort(a, low, pi - 1);
             sort(a, pi + 1, high);
-        }
-    }
-
-    // Driver method
-    public static void main(String args[]) {
-        int a[] = {10, 7, 8, 9, 1, 5};
-        int n = a.length;
-
-        SimpleQuickSort ob = new SimpleQuickSort();
-        ob.sort(a, 0, n - 1);
-
-        System.out.println("Sorted array:");
-        for (int i = 0; i < n; i++) {
-            System.out.print(a[i] + " ");
         }
     }
 }
